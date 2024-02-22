7bb70f4cf4d258eed4e4f2faa017ba23ce1bc494
tgorsk15 <tgbusiness463@gmail.com>
2024-02-22 13:31:52 -0600

    Initial commit
    Add linked-list.js file

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-/******/ (() => {
-  // webpackBootstrap
-  var __webpack_exports__ = {};
-  /*!**********************!*\
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-  console.log("Why are we still here?");
-  console.log("Yes");
+console.log("Why are we still here?");
+console.log("No");
 
-  function templateStarter() {
-    console.log("is Prettier working?");
-  }
+function templateStarter() {
+  console.log("is Prettier working?");
+}
 
-  function newList() {}
-  /******/
-})();
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7O0FBRUE7QUFDQTtBQUNBOztBQUVBO0FBQ0E7QUFDQSxFIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vbWFpbi10ZW1wbGF0ZS8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnV2h5IGFyZSB3ZSBzdGlsbCBoZXJlPycpXG5jb25zb2xlLmxvZygnWWVzJylcblxuZnVuY3Rpb24gdGVtcGxhdGVTdGFydGVyKCkge1xuICAgIGNvbnNvbGUubG9nKCdpcyBQcmV0dGllciB3b3JraW5nPycpXG59O1xuXG5mdW5jdGlvbiBuZXdMaXN0KCkge1xuICAgIFxufTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
+function newList() {
+  console.log("yay");
+}
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7O0FBRUE7QUFDQTtBQUNBOztBQUVBO0FBQ0E7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL21haW4tdGVtcGxhdGUvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJXaHkgYXJlIHdlIHN0aWxsIGhlcmU/XCIpO1xuY29uc29sZS5sb2coXCJOb1wiKTtcblxuZnVuY3Rpb24gdGVtcGxhdGVTdGFydGVyKCkge1xuICBjb25zb2xlLmxvZyhcImlzIFByZXR0aWVyIHdvcmtpbmc/XCIpO1xufVxuXG5mdW5jdGlvbiBuZXdMaXN0KCkge1xuICBjb25zb2xlLmxvZyhcInlheVwiKTtcbn1cbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
