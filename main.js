a569cbab06da5b93a470665d2c23b93f55a7e226
tgorsk15 <tgbusiness463@gmail.com>
2024-02-22 15:13:11 -0600

    Restructure js files and set up class Node and LinkedList
    The node and LinkedList classes will be separated into two different JS files

--- main.js
+++ main.js
@@ -3,17 +3,17 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-console.log("Why are we still here?");
-console.log("No");
+// console.log("Why are we still here?");
+// console.log("No");
 
-function templateStarter() {
-  console.log("is Prettier working?");
-}
+// function templateStarter() {
+//   console.log("is Prettier working?");
+// }
 
-function newList() {
-  console.log("yay");
-}
+// function newList() {
+//   console.log("yay");
+// }
 
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7O0FBRUE7QUFDQTtBQUNBOztBQUVBO0FBQ0E7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL21haW4tdGVtcGxhdGUvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJXaHkgYXJlIHdlIHN0aWxsIGhlcmU/XCIpO1xuY29uc29sZS5sb2coXCJOb1wiKTtcblxuZnVuY3Rpb24gdGVtcGxhdGVTdGFydGVyKCkge1xuICBjb25zb2xlLmxvZyhcImlzIFByZXR0aWVyIHdvcmtpbmc/XCIpO1xufVxuXG5mdW5jdGlvbiBuZXdMaXN0KCkge1xuICBjb25zb2xlLmxvZyhcInlheVwiKTtcbn1cbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7O0FBRUE7QUFDQTtBQUNBOztBQUVBO0FBQ0E7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL21haW4tdGVtcGxhdGUvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gY29uc29sZS5sb2coXCJXaHkgYXJlIHdlIHN0aWxsIGhlcmU/XCIpO1xuLy8gY29uc29sZS5sb2coXCJOb1wiKTtcblxuLy8gZnVuY3Rpb24gdGVtcGxhdGVTdGFydGVyKCkge1xuLy8gICBjb25zb2xlLmxvZyhcImlzIFByZXR0aWVyIHdvcmtpbmc/XCIpO1xuLy8gfVxuXG4vLyBmdW5jdGlvbiBuZXdMaXN0KCkge1xuLy8gICBjb25zb2xlLmxvZyhcInlheVwiKTtcbi8vIH1cbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
