7d5be8511e49a2dc7186673549ec37e97b279845
tgorsk15 <tgbusiness463@gmail.com>
2024-02-22 18:33:20 -0600

    Develop append method and start contructing prepend method

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-// console.log("Why are we still here?");
-// console.log("No");
 
-// function templateStarter() {
-//   console.log("is Prettier working?");
-// }
-
-// function newList() {
-//   console.log("yay");
-// }
-import { LinkedList } from './linked-list-class';
+import { LinkedList } from "./linked-list-class";
 
 
 const testRun = new LinkedList;
-console.log(testRun.append());
+// console.log(testRun.append());
 
 testRun.append(56);
+
+testRun.prepend(444);
--- src/linked-list-class.js
+++ src/linked-list-class.js
@@ -7,17 +7,34 @@ export class LinkedList {
     constructor (head, tail, length) {
         this.head = head;
         this.tail = tail;
-        this.length = length;
+        this.length = 0;
         this.node = Node;
     }
 
    append(chosenValue) {
-    const newNode = new Node(chosenValue);
+    const newNode = new Node();
+    console.log(newNode);
     newNode.value = chosenValue;
     console.log(newNode.value);
-    console.log(newNode);
-    return newNode
     
+
+    this.tail = newNode;
+    this.tail.nextNode = null;
+    console.log(this.tail.value);
+    console.log(this.tail.nextNode);
+
+    this.length += 1;
+    console.log(this.length) 
+    
+   }
+
+
+   prepend(chosenValue) {
+    const newNode = new Node();
+    console.log(newNode);
+    newNode.value = chosenValue;
+
+    this.head = newNode;
    }
 };
 
